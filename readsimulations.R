2d4ffdaa937f0549f90c71fabebefa41c88cb16c
Christopher Fjeldberg Jensen <chridifj@gmail.com>
2024-05-29 17:28:47 +0200

    Aber

--- readsimulations.R
+++ readsimulations.R
@@ -1,2 +1,4 @@
-d1 <- readRDS("d1_cal.rds")
-d2 <- readRDS("d2_cal.rds")
+d1_cal <- readRDS("d1_cal.rds")
+d2_cal <- readRDS("d2_cal.rds")
+d3_cal <- readRDS("d3_cal.rds")
+d1_pow <- readRDS("d1_pow.rds")
